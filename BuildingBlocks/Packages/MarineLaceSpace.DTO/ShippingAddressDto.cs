fc6e7330bd19d843268e6a36ce4f83907d633cc3
Yurii Hrohul <meinlix@ukr.net>
2025-04-15 22:07:56 +0300

    Trigger workflow run

---
+++ BuildingBlocks/Packages/MarineLaceSpace.DTO/ShippingAddressDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MarineLaceSpace.DTO;
+
+public class ShippingAddressDto
+{
+    public string FullName { get; set; }
+    public string AddressLine1 { get; set; }
+    public string AddressLine2 { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string PhoneNumber { get; set; }
+}

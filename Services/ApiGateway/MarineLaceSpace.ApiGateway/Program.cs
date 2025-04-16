3a758db185ba8618c11de9b575b7c6c3a27d9699
Yurii Hrohul <meinlix@ukr.net>
2025-04-16 17:14:36 +0300

    Add global PackageVersion for NuGet & remove not used packages

--- Services/ApiGateway/MarineLaceSpace.ApiGateway/Program.cs
+++ Services/ApiGateway/MarineLaceSpace.ApiGateway/Program.cs
@@ -3,7 +3,6 @@ using ApiGateway.WebHost.Aggregators;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 builder.Services.AddApiAggregators();
 

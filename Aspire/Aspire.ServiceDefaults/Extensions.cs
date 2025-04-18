46fd05ac48a65904321f91391ebb637b90097b64
Yurii Hrohul <meinlix@ukr.net>
2025-04-18 12:58:09 +0300

    SonarQube issues

--- Aspire/Aspire.ServiceDefaults/Extensions.cs
+++ Aspire/Aspire.ServiceDefaults/Extensions.cs
@@ -62,8 +62,6 @@ public static class Extensions
             {
                 tracing.AddSource(builder.Environment.ApplicationName)
                     .AddAspNetCoreInstrumentation()
-                    // Uncomment the following line to enable gRPC instrumentation (requires the OpenTelemetry.Instrumentation.GrpcNetClient package)
-                    //.AddGrpcClientInstrumentation()
                     .AddHttpClientInstrumentation();
             });
 
@@ -81,20 +79,12 @@ public static class Extensions
             builder.Services.AddOpenTelemetry().UseOtlpExporter();
         }
 
-        // Uncomment the following lines to enable the Azure Monitor exporter (requires the Azure.Monitor.OpenTelemetry.AspNetCore package)
-        //if (!string.IsNullOrEmpty(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]))
-        //{
-        //    builder.Services.AddOpenTelemetry()
-        //       .UseAzureMonitor();
-        //}
-
         return builder;
     }
 
     public static TBuilder AddDefaultHealthChecks<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
     {
         builder.Services.AddHealthChecks()
-            // Add a default liveness check to ensure app is responsive
             .AddCheck("self", () => HealthCheckResult.Healthy(), ["live"]);
 
         return builder;
@@ -102,14 +92,10 @@ public static class Extensions
 
     public static WebApplication MapDefaultEndpoints(this WebApplication app)
     {
-        // Adding health checks endpoints to applications in non-development environments has security implications.
-        // See https://aka.ms/dotnet/aspire/healthchecks for details before enabling these endpoints in non-development environments.
         if (app.Environment.IsDevelopment())
         {
-            // All health checks must pass for app to be considered ready to accept traffic after starting
             app.MapHealthChecks("/health");
 
-            // Only health checks tagged with the "live" tag must pass for app to be considered alive
             app.MapHealthChecks("/alive", new HealthCheckOptions
             {
                 Predicate = r => r.Tags.Contains("live")
--- BuildingBlocks/Packages/MarineLaceSpace.Enumerations/Enumeration.cs
+++ BuildingBlocks/Packages/MarineLaceSpace.Enumerations/Enumeration.cs
@@ -2,42 +2,12 @@
 
 namespace MarineLaceSpace.Enumerations;
 
-public abstract class Enumeration(int id, string name) : IComparable, IEquatable<Enumeration>
+public abstract class Enumeration(int id, string name) : IComparable,
+                                                         IEquatable<Enumeration>,
+                                                         IEqualityComparer<Enumeration>
 {
     public string Name { get; } = name;
     public int Id { get; } = id;
-
-    [Obsolete]
-    private string GetNameFromCallingMember()
-    {
-        var stackFrame = new System.Diagnostics.StackFrame(2, false);
-        var method = stackFrame.GetMethod();
-
-        if (method?.Name == ".cctor" || method?.Name == ".ctor")
-        {
-            var declaringType = method.DeclaringType;
-            if (declaringType != null)
-            {
-                var fields = declaringType.GetFields(BindingFlags.Public |
-                                                   BindingFlags.NonPublic |
-                                                   BindingFlags.Static |
-                                                   BindingFlags.DeclaredOnly);
-
-                foreach (var field in fields)
-                {
-                    if (!field.IsStatic) continue;
-
-                    var value = field.GetValue(null);
-                    if (ReferenceEquals(this, value))
-                    {
-                        return field.Name;
-                    }
-                }
-            }
-        }
-
-        throw new ArgumentNullException("name");
-    }
 
     public override string ToString() => Name;
 
@@ -70,10 +40,10 @@ public abstract class Enumeration(int id, string name) : IComparable, IEquatable
                Id == other.Id;
     }
 
-    public int CompareTo(object? other)
+    public int CompareTo(object? obj)
     {
-        if (other is null) return 1;
-        return other is Enumeration otherValue
+        if (obj is null) return 1;
+        return obj is Enumeration otherValue
                ? Id.CompareTo(otherValue.Id)
                : throw new ArgumentException($"Object must be of type {nameof(Enumeration)}");
     }
@@ -112,5 +82,23 @@ public abstract class Enumeration(int id, string name) : IComparable, IEquatable
     {
         result = FromName<T>(name);
         return result is not null;
+    }
+
+    public bool Equals(Enumeration? x, Enumeration? y)
+    {
+        if (x is null && y is null)
+            return true;
+        if (x is null || y is null)
+            return false;
+
+        return x.GetType() == y.GetType() && x.Id == y.Id;
+    }
+
+    public int GetHashCode(Enumeration obj)
+    {
+        if (obj is null)
+            return 0;
+
+        return HashCode.Combine(obj.GetType().ToString(), obj.Id);
     }
 }

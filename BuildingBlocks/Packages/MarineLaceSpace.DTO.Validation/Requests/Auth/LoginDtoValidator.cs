a6ed4b4cd60679c2356bf0566714bc6385ba3b6d
Yurii Hrohul <meinlix@ukr.net>
2025-04-20 21:49:18 +0300

    Add LoginDto validator

---
+++ BuildingBlocks/Packages/MarineLaceSpace.DTO.Validation/Requests/Auth/LoginDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MarineLaceSpace.DTO.Requests.Auth;
+
+namespace MarineLaceSpace.DTO.Validation.Requests.Auth
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(dto => dto.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Email is incorrect.");
+        }
+    }
+}
